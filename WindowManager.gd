ea13fc6ff0ed4dc0faa9871b8eb7df9659ccb6ed
Atatab67 <danielfilipe72675@gmail.com>
2024-04-20 15:10:45 -0300

    Fully finished base implementation of the Login Screen

--- WindowManager.gd
+++ WindowManager.gd
@@ -1,12 +1,16 @@
 extends Control
 
+var priviledge: int
 var text: Array
 var login: String
 var password: String
+
 func _ready() -> void:
 # Hides Everything else so only the LoginWindow Appears
 	$Windows.hide()
 	$Header.hide()
+	# Get all the Accounts from the database
+	text = $Windows/wndAdmin._read_table("type")
 
 
 func getControl(node:Control) -> Array:
@@ -48,14 +52,31 @@ func _on_btn_sair_button_down() -> void:
 
 
 func _on_btn_admin_button_down() -> void:
-	var nodes:Array = getControl($Windows)
-	nodes[3].visible = true
-	
-	for i in nodes.size():
-		if i != 3:
-			nodes[i].visible = false
-
-
+	#Check if Account has priviledge to open this page
+	if priviledge == 0:
+		var nodes:Array = getControl($Windows)
+		nodes[3].visible = true
+		
+		for i in nodes.size():
+			if i != 3:
+				nodes[i].visible = false
 
 func _on_login_button_button_down():
-	pass # Replace with function body.
+	#Get Input from the user
+		login = $LoginWindow/UsernameBox.text
+		password = $LoginWindow/PasswordBox.text  
+		#Loop throught all the logins
+		for i in text.size():
+			#If login as email or username is correct
+			if login == text[i]['email'] || login == text[i]['username']:
+				#Password Check
+				if password == text[i]['password']:
+					#Hide login and turn everything back on
+					$LoginWindow.hide()
+					$Windows.show()
+					$Header.show()
+					#Set priviledge as the one from the account
+					priviledge = text[i]['type']
+			else:
+			#Wrong Login
+				print("Incorrect.")
