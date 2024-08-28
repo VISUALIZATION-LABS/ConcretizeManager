ddcd133b4916b5db1c0e9e50bc33e978fc3450b9
danielfilipe41 <danielfilipe72675@gmail.com>
2024-08-27 23:17:44 -0300

    Upadet!!!!!!!!! þrojects and more!!!!!

--- Scripts/WindowManager.gd
+++ Scripts/WindowManager.gd
@@ -8,8 +8,8 @@ var password: String
 
 func _ready() -> void:
 #  WARNING Hides Everything else so only the LoginWindow Appears
-	#$Windows.hide()
-	#$Header.hide()
+	$Windows.hide()
+	$Header.hide()
 	# Get all the Accounts from the database
 	text = $Windows/wndAdmin._read_table("type")
 	#Set All Accounts as Options on the AccountSelector
@@ -27,30 +27,28 @@ func getControl(node:Control) -> Array:
 
 func _on_btn_install_button_down() -> void:
 	var nodes:Array = getControl($Windows)
-	nodes[0].visible = true
-	
-	for i in nodes.size():
-		if i != 0:
-			nodes[i].visible = false
-
-func _on_btn_pacotes_button_down() -> void:
-	var nodes:Array = getControl($Windows)
 	nodes[1].visible = true
 	
 	for i in nodes.size():
 		if i != 1:
 			nodes[i].visible = false
 
+func _on_btn_pacotes_button_down() -> void:
+	var nodes:Array = getControl($Windows)
+	nodes[2].visible = true
+	
+	for i in nodes.size():
+		if i != 2:
+			nodes[i].visible = false
+
 
 func _on_btn_conta_button_down() -> void:
-	#Check if Login is not a TempUser
-	if priviledge != 3:
-		var nodes:Array = getControl($Windows)
-		nodes[2].visible = true
-		
-		for i in nodes.size():
-			if i != 2:
-				nodes[i].visible = false
+	var nodes:Array = getControl($Windows)
+	nodes[3].visible = true
+	
+	for i in nodes.size():
+		if i != 3:
+			nodes[i].visible = false
 
 
 func _on_btn_sair_button_down() -> void:
@@ -61,10 +59,10 @@ func _on_btn_admin_button_down() -> void:
 	#Check if Login is admin so he can open this page
 	if priviledge == 0:
 		var nodes:Array = getControl($Windows)
-		nodes[3].visible = true
+		nodes[4].visible = true
 		
 		for i in nodes.size():
-			if i != 3:
+			if i != 4:
 				nodes[i].visible = false
 
 func _on_login_button_button_down() -> void:
