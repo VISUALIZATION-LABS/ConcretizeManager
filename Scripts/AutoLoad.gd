e96cbe31dd84a70d94cffa0404d481de561e2bd9
Daniel Filipe <75042372+danielfilipe41@users.noreply.github.com>
2024-11-21 12:15:29 -0300

    last commit for now

--- Scripts/AutoLoad.gd
+++ Scripts/AutoLoad.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	database.path="res://data.db"
 	database.open_db()
 	directories = dir.get_directories()
-	#Be Certain that Package and Projects Directories exist
+	#Be Certain that the Directories exist
 	print(directories)
 	if directories.find("Projects") != -1:
 		print("Found Directory Projects")
@@ -30,6 +30,11 @@ func _ready() -> void:
 	else:
 		print("Making Directory Packages")
 		dir.make_dir("Projects")
+	if directories.find("Documentation") != -1:
+		print("Found Directory Documentation")
+	else:
+		print("Making Directory Documentation")
+		dir.make_dir("Documentation")
 	#Opening access to them after we are sure that they exist
 	projectdir = DirAccess.open(OS.get_user_data_dir() + "/Projects")
 	packagedir = DirAccess.open(OS.get_user_data_dir() + "/Packages")
